703e6e079d7f8537fdb429948162bb5c36b75865
romainhild <romain.hild@me.com>
2022-02-07 21:10:13 +0100

    Initial Commit

---
+++ YamahaController/YamahaControllerApp.swift
@@ -0,0 +1,17 @@
+//
+//  YamahaControllerApp.swift
+//  YamahaController
+//
+//  Created by Romain Hild on 07/02/2022.
+//
+
+import SwiftUI
+
+@main
+struct YamahaControllerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
